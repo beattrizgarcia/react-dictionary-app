b0d125bbbe6d6949f2d037473665da2f60ac186b
beattrizgarcia <159271671+beattrizgarcia@users.noreply.github.com>
2024-08-05 14:56:03 +0100

    Added the basic search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <div className="container">
+        <header></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Beatriz Garcia</footer>
+      </div>
     </div>
   );
 }
