73b219b2bbd327f2f876fce5d15064bd2c32b4e1
beattrizgarcia <159271671+beattrizgarcia@users.noreply.github.com>
2024-08-06 11:39:52 +0100

    Added the results component and displayed the results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiKey = "f55d396ae54o7a80b1e5dfc1t9302b80";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -33,6 +35,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         ></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Results.css";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <div>
+          <h2>{props.results.word}</h2>
+          <h3> {props.results.meanings[0].partOfSpeech}</h3>
+          <p> {props.results.meanings[0].definition} </p>
+        </div>
+        <div>
+          <h3> {props.results.meanings[1].partOfSpeech}</h3>
+          <p> {props.results.meanings[1].definition} </p>
+        </div>
+        <div>
+          <h3> {props.results.meanings[2].partOfSpeech}</h3>
+          <p> {props.results.meanings[2].definition} </p>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
