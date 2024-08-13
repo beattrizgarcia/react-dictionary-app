0ba6bc9f47f0b67875efdb3903534ae022eef152
beattrizgarcia <159271671+beattrizgarcia@users.noreply.github.com>
2024-08-13 11:07:19 +0100

    Added the synonyms part with conditional rendering

--- src/Results.js
+++ src/Results.js
@@ -8,19 +8,52 @@ export default function Results(props) {
         <div>
           <div>
             <h2>{props.results.word}</h2>
-            <h3> {props.results.meanings[0].partOfSpeech}</h3>
-            <p> {props.results.meanings[0].definition} </p>
+            {props.results.meanings[0] && (
+              <div>
+                <h3> {props.results.meanings[0].partOfSpeech}</h3>
+                <strong>Definition:</strong>
+                <p> {props.results.meanings[0].definition} </p>
+                <strong>Example:</strong>
+                <p>{props.results.meanings[0].example}</p>
+
+                {props.results.meanings[0].synonyms && (
+                  <div>
+                    <strong>Synonyms:</strong>
+                    <p>{props.results.meanings[0].synonyms}</p>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
           {props.results.meanings[1] && (
             <div>
               <h3> {props.results.meanings[1].partOfSpeech}</h3>
+              <strong>Definition:</strong>
               <p> {props.results.meanings[1].definition} </p>
+              <strong>Example:</strong>
+              <p>{props.results.meanings[1].example}</p>
+
+              {props.results.meanings[1].synonyms && (
+                <div>
+                  <strong>Synonyms:</strong>
+                  <p>{props.results.meanings[1].synonyms}</p>
+                </div>
+              )}
             </div>
           )}
           {props.results.meanings[2] && (
             <div>
               <h3> {props.results.meanings[2].partOfSpeech}</h3>
+              <strong>Definition:</strong>
               <p> {props.results.meanings[2].definition} </p>
+              <strong>Example:</strong>
+              <p>{props.results.meanings[2].example}</p>
+              {props.results.meanings[2].synonyms && (
+                <div>
+                  <strong>Synonyms:</strong>
+                  <p>{props.results.meanings[2].synonyms}</p>
+                </div>
+              )}
             </div>
           )}
         </div>
